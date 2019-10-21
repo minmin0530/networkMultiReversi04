a94316666c47cf2f3f73797b2c57393c8e712494
izumi_yoshiki <izumiyoshiki@icloud.com>
2019-10-21 19:12:49 +0900

    correction src

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ var watchingNumber = 0;
 var socketID = [];
 var field = [];
 var turn = [];
-
+var fieldOwner = [];
+var players = [];
+var fieldNumberArray = [];
 //初期化（オセロ盤と順番の初期化）
 for (var v = 0; v < 3; ++v) {
     var arrayY = [];
@@ -36,9 +38,10 @@ io.sockets.on("connection", function (socket) {
     socketID.push( socket.id );
     var getNameData = {
       'name': name,
+      'fieldNumberArray': fieldNumberArray,
       'field': field
     };
-    io.sockets.emit("getName", {value: getNameData});
+    io.sockets.connected[socket.id].emit("getName", {value: getNameData});
     ++watchingNumber;
 
     // 誰かがコマを置いた処理をクライアントから受け取り
@@ -54,9 +57,65 @@ io.sockets.on("connection", function (socket) {
         };
 
         // 全員にコマを置いた処理を送信
-        io.sockets.emit("put", {value:d});
+        io.sockets.to(data.value.fieldNumber).emit("put", {value:d});
     });
 
+    // テーブルのオーナー作成者を決める
+    socket.on("fieldOwner", function(data) {
+      let i = 0;
+      let existFlag = true;
+      for (const fn of fieldNumberArray) {
+        if (fn == 3) {
+          fieldNumberArray[i] = 1;
+          fieldOwner[i] = data.fieldOwner;
+          existFlag = false;
+          break;
+        }
+        ++i;
+      }
+      if (existFlag) {
+        fieldNumberArray.push(1);
+        fieldOwner[fieldNumberArray.length - 1] = data.fieldOwner;
+      }
+      players.push(data.fieldOwner);
+      if (existFlag) {
+        socket.join(fieldNumberArray.length - 1);
+        io.sockets.connected[socketID[ fieldOwner[fieldNumberArray.length - 1] ]].emit("currentTable", fieldNumberArray.length - 1);      
+      } else {
+        socket.join(i);
+        io.sockets.connected[socketID[ fieldOwner[i] ]].emit("currentTable", i);
+      }
+    });
+
+    // 既にあるテーブルへ参加する
+    socket.on("join", function(data) {
+      let sameFlag = false;
+      for (const player of players) {
+        if (player == data.myName) {
+          sameFlag = true;
+        }
+      }
+      if (sameFlag == false) {
+        players.push(data.myName);
+      }
+
+      const joinData = {
+        number: players.length
+      };
+      socket.join(data.fieldNumber);
+      io.sockets.connected[socketID[ fieldOwner[data.fieldNumber] ]].emit("join", joinData);
+    });
+
+    // ゲーム開始
+    socket.on("startGame", function(data) {
+      if (fieldNumberArray[data] == 1) {
+        fieldNumberArray[data] = 2;
+      }
+
+      io.sockets.emit("startGame", data);
+    });
+
+    // 離脱
     socket.on("disconnect", function () {
       if (userHash[socket.id]) {
         delete userHash[socket.id];
